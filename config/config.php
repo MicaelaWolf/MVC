a41f8566f7d9f9dcd01b7c5bcba69b634667a9ef
Micaela Wolf <angimica99@gmail.com>
2023-06-05 18:51:45 -0300

    mvc

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php 
+define('URL', 'http://' . $_SERVER['HTTP_HOST'] . '/exsol/testDep/nocheBC/');
+
+//conexion a la base de datos
+define('HOST', 'localhost');
+define('PORT_DB', '3307');
+define('DB', 'e01');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+use Micaela\mvc\libs\App;
+
+//use mvc\models\Auto;
+require 'vendor/autoload.php';
+require 'config/config.php';
