df36ff1646b05f57f3c41a67ce5499546d017aff
Emanuel <emabarnine@gmail.com>
2023-07-03 18:38:40 -0300

    singleton

--- src/Libs/Singleton.php
+++ src/Libs/Singleton.php
@@ -1,6 +1,35 @@
 <?php 
 namespace Micaela\App\Libs;
 
+use PDO;
+
 class Singleton {
+  private static $instancia=null;
+  private $pdo;
+  private function __construct(){
+    $host     = constant('HOST');
+    $port     = constant('PORT_DB');
+    $db       = constant('DB');
+    $user     = constant('USER');
+    $password = constant('PASSWORD');
+    $charset  = constant('CHARSET');
+
+    $connection = "mysql:host={$host};port={$port};dbname={$db};charset={$charset}";
+    $options    = [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_EMULATE_PREPARES => false,
+    ];
+     $this->pdo = new PDO($connection, $user, $password, $options);
+  }
   
-}
+  public static function getInstancia() {
+    if (!isset(self::$instancia)){
+      self::$instancia=new Singleton();
+    }
+    return self::$instancia;
+  }
+  
+  public function getPdo() {
+    return self::$instancia->pdo;
+  }
+} 
